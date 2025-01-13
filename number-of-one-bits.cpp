b89d7910a21e2a8be8ddf824fa57a7dd641bb652
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-01-13 05:06:39 -0500

    Create number-of-one-bits.cpp

---
+++ number-of-one-bits.cpp
@@ -0,0 +1,14 @@
+//https://neetcode.io/problems/number-of-one-bits
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        while(n){
+            if(n&1)
+                count++;
+            n = n>>1;
+        }
+        return count;
+    }
+};
